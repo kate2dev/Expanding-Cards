cc9af2998b34cb01879e24dfd30cc41f2e2bb0d7
kate Emoefe <eseemoefe@gmail.com>
2024-06-12 17:58:31 +0100

    project

---
+++ index.js
@@ -0,0 +1,13 @@
+const pannels = document.querySelectorAll('.panel')
+pannels.forEach((panel) => {
+    panel.addEventListener('click',() => {
+        removeActiveClasses()
+        panel.classList.add('active')
+    })
+})
+
+function removeActiveClasses() {
+    pannels.forEach(panel => {
+        panel.classList.remove('active')
+    })
+}
